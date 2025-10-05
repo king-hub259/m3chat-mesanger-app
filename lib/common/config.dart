71804f6997b199ffeb4438f0b039e7bac0252c3b
king-hub259 <84419911+king-hub259@users.noreply.github.com>
2025-10-05 13:36:43 -0700

    Update config.dart

--- lib/common/config.dart
+++ lib/common/config.dart
@@ -8,11 +8,11 @@
 /* =============================== */
 
 // Sngine API Key & Secret
-const String apiKey = "";
-const String apiSecret = "";
+const String apiKey = "97b76f6314af9ad4a42003dfd80a87284c7767173bab6934";
+const String apiSecret = "2acc694ef4e6abcd340b73bc9bb1af74924431f2ee3684792f113125d99da739";
 
 // Sngine API URL
-const String apiURL = "https://demo.sngine.com";
+const String apiURL = "https://m3chat.com.ng";
 
 // Debugging Mode
 const bool debugEnabled = false;
@@ -22,7 +22,7 @@ const bool debugEnabled = false;
 /* =============================== */
 
 // OneSignal App ID
-const String oneSignalAppId = "";
+const String oneSignalAppId = "fad3f3d1-13c4-4c0d-92d1-b41cdf1e56d9";
 
 /* =============================== */
 /* 🛑 DON'T EDIT BELOW THIS LINE 🛑 */
