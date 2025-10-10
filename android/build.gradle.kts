e87607e653026f0be67bce78226230df313ebfc4
king-hub259 <84419911+king-hub259@users.noreply.github.com>
2025-10-10 11:19:42 -0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,24 +1,18 @@
+buildscript {
+    ext.kotlin_version = "1.9.25"
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath "com.android.tools.build:gradle:8.5.2"
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+    }
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
-}
-
-// ⚠️ Remove or comment out custom build directory overrides for Codemagic
-// val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-// rootProject.layout.buildDirectory.value(newBuildDir)
-//
-// subprojects {
-//     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-//     project.layout.buildDirectory.value(newSubprojectBuildDir)
-// }
-
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-// Standard clean task
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
 }
