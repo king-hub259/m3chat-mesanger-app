a43ba0c24f600d9842244c115d43b3308b46bfa8
king-hub259 <84419911+king-hub259@users.noreply.github.com>
2025-10-10 11:43:35 -0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,12 +1,20 @@
+// android/build.gradle.kts
+
+import org.gradle.kotlin.dsl.*
+
+plugins {
+    // no plugins here for top-level file
+}
+
 buildscript {
-    ext.kotlin_version = "1.9.25"
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:8.5.2"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:8.5.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
+        classpath("dev.flutter:flutter-gradle-plugin:1.0.0")
     }
 }
 
@@ -15,4 +23,8 @@ allprojects {
         google()
         mavenCentral()
     }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
